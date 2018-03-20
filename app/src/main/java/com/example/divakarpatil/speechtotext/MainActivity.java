d07f4f91dafc2fe61c9e8de535c1b17161715ca7
Divakar Patil <divakar.patil@globant.com>
2018-03-21 00:18:50 +0530

    Changes made: 1. Updated code for JSON file reading 2. Added support for next and previous button

--- app/src/main/java/com/example/divakarpatil/speechtotext/MainActivity.java
+++ app/src/main/java/com/example/divakarpatil/speechtotext/MainActivity.java
@@ -1,8 +1,11 @@
 package com.example.divakarpatil.speechtotext;
 
 import android.Manifest;
+import android.app.AlertDialog;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.content.pm.PackageManager;
+import android.graphics.Color;
 import android.os.Bundle;
 import android.os.CountDownTimer;
 import android.speech.RecognitionListener;
@@ -13,6 +16,9 @@ import android.support.v4.app.ActivityCompat;
 import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.text.SpannableString;
+import android.text.SpannableStringBuilder;
+import android.text.style.ForegroundColorSpan;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
@@ -26,10 +32,13 @@ import static com.example.divakarpatil.speechtotext.R.string.speech_record_stop;
 public class MainActivity extends AppCompatActivity implements RecognitionListener {
 
     private static final String TAG = MainActivity.class.getSimpleName();
-    private static final int MY_PERMISSIONS_REQUEST_READ_CONTACTS = 100;
-    private TextView timerTextView, speechToTextConversionBox, sentencesTextView, accuracyPercentageView;
-    private Button speechToTextButton;
+    private static final int MY_PERMISSIONS_REQUEST_RECORD_VOICE = 100;
+    private static int currentParagraph = 0;
+    private TextView timerTextView, sentencesTextView, accuracyPercentageView, paragraphNumberTextView;
+    private Button speechToTextButton, showRecordButton, nextButton, previousButton;
     private SpeechRecognizer recognizer;
+    private ArrayList<String> matches = new ArrayList<>(1);
+    private SpannableStringBuilder spannableStringBuilder = new SpannableStringBuilder();
     private CountDownTimer timer = new CountDownTimer(40000, 1) {
         @Override
         public void onTick(long millisUntilFinished) {
@@ -50,15 +59,51 @@ public class MainActivity extends AppCompatActivity implements RecognitionListen
         setContentView(R.layout.activity_main);
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
-        speechToTextConversionBox = findViewById(R.id.speechRecordingTextView);
 
+        accuracyPercentageView = findViewById(R.id.accuracyPercentage);
         sentencesTextView = findViewById(R.id.sentencesTextView);
-        sentencesTextView.setText(R.string.sample_sentence);
+        previousButton = findViewById(R.id.previousButton);
+        nextButton = findViewById(R.id.nextButton);
+        paragraphNumberTextView = findViewById(R.id.paragraphNumberTextView);
+
+        SentencesJsonReader.readJson(this);
+        previousButton.setEnabled(false);
+        sentencesTextView.setText(SentencesJsonReader.getParagraph(currentParagraph));
 
         timerTextView = findViewById(R.id.timerTextView);
 
         speechToTextButton = findViewById(R.id.speechRecorderButton);
         speechToTextButton.setOnClickListener(getSpeechToTextListener());
+
+        showRecordButton = findViewById(R.id.showRecordTextButton);
+        showRecordButton.setEnabled(false);
+        showRecordButton.setOnClickListener(getShowRecordButtonListener());
+
+        handlePermission();
+
+        nextButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                sentencesTextView.setText(SentencesJsonReader.getParagraph(++currentParagraph));
+                previousButton.setEnabled(currentParagraph != 0);
+                nextButton.setEnabled(SentencesJsonReader.getJsonDataSize() != currentParagraph);
+                paragraphNumberTextView.setText(String.format(Locale.getDefault(), "%d", currentParagraph));
+            }
+        });
+
+        previousButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                sentencesTextView.setText(SentencesJsonReader.getParagraph(--currentParagraph));
+                previousButton.setEnabled(currentParagraph != 0);
+                nextButton.setEnabled(SentencesJsonReader.getJsonDataSize() != currentParagraph);
+                paragraphNumberTextView.setText(String.format(Locale.getDefault(), "%d", currentParagraph));
+            }
+        });
+    }
+
+    private void handlePermission() {
+
         if (ContextCompat.checkSelfPermission(this,
                 Manifest.permission.RECORD_AUDIO)
                 != PackageManager.PERMISSION_GRANTED) {
@@ -68,24 +113,26 @@ public class MainActivity extends AppCompatActivity implements RecognitionListen
             // sees the explanation, try again to request the permission.
             ActivityCompat.requestPermissions(this,
                     new String[]{Manifest.permission.RECORD_AUDIO},
-                    MY_PERMISSIONS_REQUEST_READ_CONTACTS);
+                    MY_PERMISSIONS_REQUEST_RECORD_VOICE);
         }
+    }
+
+    @NonNull
+    private View.OnClickListener getShowRecordButtonListener() {
+        return new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                AlertDialog.Builder builder = new AlertDialog.Builder(MainActivity.this);
+                builder.setMessage(spannableStringBuilder).setTitle(R.string.app_name);
+                AlertDialog dialog = builder.create();
+                dialog.show();
+            }
+        };
     }
 
     @Override
     public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
         super.onRequestPermissionsResult(requestCode, permissions, grantResults);
-        switch (requestCode) {
-            case MY_PERMISSIONS_REQUEST_READ_CONTACTS: {
-                // If request is cancelled, the result arrays are empty.
-                if (grantResults.length > 0
-                        && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
-
-                } else {
-                    // permission denied, boo! Disable the functionality that depends on this permission.
-                }
-            }
-        }
     }
 
     @NonNull
@@ -96,13 +143,14 @@ public class MainActivity extends AppCompatActivity implements RecognitionListen
                 if (speechToTextButton.getText().equals("Start Recording")) {
                     timer.start();
                     speechToTextButton.setText(speech_record_stop);
-                    speechToTextConversionBox.setText("");
                     accuracyPercentageView.setText("");
+                    spannableStringBuilder = new SpannableStringBuilder();
                     startVoiceInput();
                 } else {
                     timer.cancel();
                     speechToTextButton.setText(R.string.speech_record_start);
                     recognizer.stopListening();
+                    showRecordButton.setEnabled(false);
                 }
             }
         };
@@ -118,6 +166,7 @@ public class MainActivity extends AppCompatActivity implements RecognitionListen
         Intent intent = new Intent(RecognizerIntent.ACTION_RECOGNIZE_SPEECH);
         intent.putExtra(RecognizerIntent.EXTRA_LANGUAGE_MODEL, RecognizerIntent.LANGUAGE_MODEL_FREE_FORM);
         intent.putExtra(RecognizerIntent.EXTRA_LANGUAGE, Locale.getDefault());
+        intent.putExtra(RecognizerIntent.EXTRA_MAX_RESULTS, 10);
 
         recognizer = SpeechRecognizer.createSpeechRecognizer(this);
         recognizer.setRecognitionListener(this);
@@ -126,54 +175,70 @@ public class MainActivity extends AppCompatActivity implements RecognitionListen
 
     @Override
     public void onReadyForSpeech(Bundle params) {
-        Log.i(TAG, "onReadyForSpeech:");
     }
 
     @Override
     public void onBeginningOfSpeech() {
-        Log.i(TAG, "onBeginningOfSpeech: ");
     }
 
     @Override
     public void onRmsChanged(float rmsdB) {
-        Log.i(TAG, "onRmsChanged: ");
     }
 
     @Override
     public void onBufferReceived(byte[] buffer) {
-        Log.i(TAG, "onBufferReceived: ");
     }
 
     @Override
     public void onEndOfSpeech() {
-        Log.i(TAG, "onEndOfSpeech: ");
     }
 
     @Override
     public void onError(int error) {
         Log.i(TAG, "onError: " + error);
+        if (error == SpeechRecognizer.ERROR_CLIENT) {
+            AlertDialog.Builder builder = new AlertDialog.Builder(MainActivity.this);
+            builder.setMessage("Error while recording audio");
+            final AlertDialog alertDialog = builder.create();
+            alertDialog.show();
+            builder.setNeutralButton("", new DialogInterface.OnClickListener() {
+                @Override
+                public void onClick(DialogInterface dialog, int which) {
+                    alertDialog.cancel();
+                }
+            });
+
+        }
     }
 
     @Override
     public void onResults(Bundle results) {
         Log.i(TAG, "onResults: ");
-        ArrayList<String> matches = results.getStringArrayList(SpeechRecognizer.RESULTS_RECOGNITION);
-        if (matches == null) {
+        matches = results.getStringArrayList(SpeechRecognizer.RESULTS_RECOGNITION);
+        if (matches == null || speechToTextButton.getText().equals("Stop Recording")) {
             return;
         }
-        speechToTextConversionBox.setText(matches.get(0));
 
-        String[] originalText = speechToTextConversionBox.getText().toString().toLowerCase().split(" ");
-        String[] myText = sentencesTextView.getText().toString().toLowerCase().split(" ");
+        String[] myText = matches.get(0).toLowerCase().split(" ");
+        String[] originalText = sentencesTextView.getText().toString().toLowerCase().split(" ");
         int correctWords = 0;
+
         for (int i = 0; i < originalText.length; i++) {
-            if (originalText[i].equals(myText[i])) {
-                correctWords++;
+            try {
+                SpannableString string = new SpannableString(myText[i] + " ");
+                if (originalText[i].equals(myText[i])) {
+                    correctWords++;
+                } else {
+                    string.setSpan(new ForegroundColorSpan(Color.RED), 0, myText[i].length(), 0);
+                }
+                spannableStringBuilder.append(string);
+            } catch (ArrayIndexOutOfBoundsException e) {
+                break;
             }
         }
         double accuracy = ((double) correctWords / (double) originalText.length) * 100;
-        accuracyPercentageView = findViewById(R.id.accuracyPercentage);
         accuracyPercentageView.setText(String.format(Locale.getDefault(), "%.2f", accuracy));
+        showRecordButton.setEnabled(true);
     }
 
     @Override
--- app/src/main/java/com/example/divakarpatil/speechtotext/SentencesJsonReader.java
+++ app/src/main/java/com/example/divakarpatil/speechtotext/SentencesJsonReader.java
@@ -1,31 +1,44 @@
 package com.example.divakarpatil.speechtotext;
 
+import android.content.Context;
 import android.util.JsonReader;
 
 import java.io.BufferedReader;
-import java.io.File;
-import java.io.FileNotFoundException;
-import java.io.FileReader;
 import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
 import java.util.ArrayList;
 
 /**
  * Json Reader class for Sentences
- * Created by divakar.patil on 14-03-2018.
+ * <p>
+ * Created by Divakar Patil on 14-03-2018.
  */
 
-public class SentencesJsonReader {
+class SentencesJsonReader {
+
     private static ArrayList<String> jsonArray = new ArrayList<>();
 
-    public static void readJson() throws FileNotFoundException {
-        JsonReader reader = new JsonReader(new BufferedReader(new FileReader(new File("/sentences.json"))));
-        try {
+    static void readJson(Context context) {
+        try (InputStream inputStream = context.getAssets().open("sentences.json");
+             JsonReader reader = new JsonReader(new BufferedReader(new InputStreamReader(inputStream)))) {
+
             reader.beginObject();
             while (reader.hasNext()) {
+                reader.nextName();
                 jsonArray.add(reader.nextString());
             }
+            reader.endObject();
         } catch (IOException e) {
             e.printStackTrace();
         }
+    }
+
+    static String getParagraph(int i) {
+        return jsonArray.get(i);
+    }
+
+    static int getJsonDataSize() {
+        return jsonArray.size() - 1;
     }
 }
